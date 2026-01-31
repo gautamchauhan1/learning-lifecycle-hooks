4f14db3c7766dd3005913bc3d94b9ad4bf81d77e
gautamchauhan1 <87991548+gautamchauhan1@users.noreply.github.com>
2026-01-31 12:47:49 +0530

    feat: manual subscription, unsubscribe observable and async pipe

--- src/app/life-child/life-child.component.ts
+++ src/app/life-child/life-child.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Observable, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-life-child',
@@ -7,28 +8,56 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class LifeChildComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+
 
   count: number =0;
   timer: any;
+  mySubscription!: Subscription;
+  manualVal: number =0;
+  dataStream$ : Observable<number>;
+
+
+  constructor() {
+      // We just define the stream. We DO NOT subscribe 
+
+      this.dataStream$ = interval(1000).pipe(tap(value=> console.log(value)
+      ))
+      
+     }
 
   ngOnChanges(changes: SimpleChanges): void {
 
   }
 
-
   ngOnInit(): void {
+   // setInterval and clearInterval
+
     this.timer = setInterval(()=>{
       this.count++;
     }, 1000
     );
     console.log('Timer started');
-    
+
+   // Manual subscription
+
+   this.mySubscription = interval(1000).subscribe((value)=>
+  { 
+    this.manualVal = value;
+  })
+
   }
 
     ngOnDestroy(): void {
     clearInterval(this.timer);
+
+    // 🛑 CRITICAL: We manually Unsubscribe here
+    if(this.mySubscription)
+    {
+      this.mySubscription.unsubscribe();
+    }
     console.log('timer stopped, Component destroyed');
+
+    // We do not write code here for destroy component.
     
   }
 
